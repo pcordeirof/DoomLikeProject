8087b153a4e78b545e54f738b3dae018982eca35
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2021-02-24 15:11:00 -0300

    Inimigo recebendo dano

---
+++ DoomLikeProject/Assets/Scripts/AI/HPManager.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+
+public class HPManager : MonoBehaviour
+{
+    public FloatVariable maxHP;
+    public Slider slider;
+    public float HP;
+    public TakeDamage takeDamage;
+
+    void Start()
+    {
+        slider.maxValue = maxHP.Value;
+        slider.value = maxHP.Value;
+
+        HP = maxHP.Value;
+    }
+
+
+    public void DecreaseHP()
+    {
+        HP--;
+        slider.value = HP;
+        if(HP <= 0)
+        {
+            takeDamage.EnemyKilled();
+        }
+    }
+}
---
+++ DoomLikeProject/Assets/Scripts/AI/TakeDamage.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TakeDamage : MonoBehaviour
+{
+    public Camera cameraMain;
+    RaycastHit hitInfo = new RaycastHit();
+    public Collider enemyCollider;
+    public HPManager hPManager;
+
+    private void Start()
+    {
+        cameraMain = Camera.main;
+    }
+    void Update()
+    {
+        if(Input.GetMouseButtonDown(0))
+        {
+            Ray ray = cameraMain.ScreenPointToRay(Input.mousePosition);
+            Physics.Raycast(ray.origin, ray.direction, out hitInfo);
+            
+            if(hitInfo.collider == enemyCollider)
+            {
+                Debug.Log("Hit!");
+                hPManager.DecreaseHP();
+            }   
+            
+        }
+    }
+
+    public void EnemyKilled()
+    {
+        Destroy(gameObject);
+    }
+}
