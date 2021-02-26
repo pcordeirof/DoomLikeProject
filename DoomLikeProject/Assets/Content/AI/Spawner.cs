ea2757be8d51daaf25581e00f055d2e8632304a3
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2021-02-26 20:27:04 -0300

    Sistema de spawn manual

---
+++ DoomLikeProject/Assets/Content/AI/Spawner.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+public class Spawner : MonoBehaviour
+{
+    public GameObject enemyPrefab;
+    public PossiblesPositions possiblesPositions;
+    public bool spawnDuringStart;
+    void Start()
+    {
+        if (spawnDuringStart)
+        {
+            for (int i = 0; i < 5; i++)
+            {
+                SpawnNewEnemy();
+            }
+        }
+       
+    }
+    
+    [Button("Spawn New Enemy")]
+    void SpawnNewEnemy()
+    {
+        GameObject newEnemy;
+        newEnemy = Instantiate(enemyPrefab, possiblesPositions.EnemyPossiblesPositions[Random.Range(0 , possiblesPositions.EnemyPossiblesPositions.Count)], Quaternion.identity);
+    }
+}
---
+++ DoomLikeProject/Assets/ScriptableObjects/PossiblesPositions.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Sirenix.OdinInspector;
+
+[InlineEditor]
+[CreateAssetMenu(fileName = "EnemyPossiblesPositions", menuName = "Enemy/EnemyPossiblesPositions")]
+public class PossiblesPositions: ScriptableObject
+{
+    public List<Vector3> EnemyPossiblesPositions = new List<Vector3>();
+    public FloatVariable Height;
+
+    [Button("Add Position")]
+    public void AddPositon(float x,  float z)
+    {
+        Vector3 position = new Vector3(x, Height.Value, z);
+        EnemyPossiblesPositions.Add(position);
+    }
+
+    [Button("Remove Position")]
+    public void RemovePositon(float x, float z)
+    {
+        Vector3 position = new Vector3(x, Height.Value, z);
+        EnemyPossiblesPositions.Remove(position);
+    }
+
+    [Button("Clean Positions")]
+    public void CleanPositions()
+    {
+        EnemyPossiblesPositions.Clear();
+    }
+}
