5215987a1d54aecf0c163a788466d8335f2ec768
Pedro Cordeiro Ferreira <pedrocf96@hotmail.com>
2021-04-05 17:41:06 -0300

    Shot Animation

---
+++ DoomLikeProject/Assets/Scripts/Player/GunRecoil.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using DG.Tweening;
+using UnityEngine.UI;
+
+public class GunRecoil : MonoBehaviour
+{
+    public Animator handGunAnimator;
+    void Update()
+    {
+        if (Input.GetMouseButtonDown(0))
+        {
+            handGunAnimator.SetTrigger("Shot");
+        }
+    }
+
+}
